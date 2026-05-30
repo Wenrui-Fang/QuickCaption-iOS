20f27da70aa9aa3d9cf127951bc0de05255de537
fangwenrui <fangwenrui1997@outlook.com>
2026-05-30 15:14:01 -0700

    Initial Commit

---
+++ QuickCaption/QuickCaptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCaptionApp.swift
+//  QuickCaption
+//
+//  Created by fangwenrui on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickCaptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
